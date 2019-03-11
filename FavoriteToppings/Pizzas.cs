be0cbdf694cf7a8d6516287662017bc81bce30f0
MCrank <marco_mcse@hotmail.com>
2019-03-11 17:43:38 -0500

    Working with help from Gunny

---
+++ FavoriteToppings/Pizzas.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FavoriteToppings
+{
+    public class Pizzas
+    {
+        [JsonProperty("Toppings")]
+        public List<string> Toppings { get; set; }
+
+        public override string ToString()
+        {
+            if (Toppings == null || !Toppings.Any())
+            {
+                return "No toppings";
+            }
+
+            //return Toppings.OrderBy(x => x).Aggregate((a, x) => a + ", " + x);
+
+            return string.Join(", ", Toppings.OrderBy(x => x));
+        
+        }
+    }
+
+}
--- FavoriteToppings/Program.cs
+++ FavoriteToppings/Program.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace FavoriteToppings
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HttpClient client = new HttpClient();
+            string json = await GetJsonAsync(client);
+
+            var pizzas = JsonConvert.DeserializeObject<List<Pizzas>>(json);
+            Console.WriteLine($"Found {pizzas.Count} pizzas");
+
+            var pizzaStats = pizzas
+                .Select(p => p.ToString())
+                .GroupBy(p => p)
+                .OrderByDescending(t => t.Count())
+                .Select(s => new { s.Key, Number = s.Count() });
+
+            var top20Pizzas = pizzaStats.Take(20);
+
+            Console.WriteLine("\nStats of the Top 20 Pizzas and Toppings\n");
+
+            foreach (var pizza in top20Pizzas)
+            {
+                Console.WriteLine("\tCount: {0,5}, \t\tToppings {1}", pizza.Number, pizza.Key);
+            }
+
+            Console.ReadLine();
+
+        }
+        static async Task<string> GetJsonAsync(HttpClient client)
+        {
+            var result = await client.GetStringAsync("http://files.olo.com/pizzas.json");
+
+            return result;
         }
     }
 }
